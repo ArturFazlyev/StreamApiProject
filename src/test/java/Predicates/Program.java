3d81afb17d9bf680b460322b3c486bc12b88fcfa
Артур <fazlyev.art@gmail.com>
2021-01-17 12:55:20 +0500

    new commit

--- src/test/java/Predicates/Program.java
+++ src/test/java/Predicates/Program.java
@@ -6,21 +6,24 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        List<Person> nums = new ArrayList<>();
-        nums.add(new Person("Cергей", 12));
-        nums.add(new Person("Лада", 13));
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Cергей", 20));
+        persons.add(new Person("Лада", 20));
 
 
         // Печать коллекции
-        nums.stream().forEach(System.out::println);
+        persons.stream().forEach(System.out::println);
         // Перебор коллекции
-        nums.stream().filter(person -> person.getAge() >=18).forEach(System.out::println);
+        persons.stream().filter(person -> person.getAge() >= 18).forEach(System.out::println);
 
-        nums.stream().filter(person -> person.getAge() >=18).sorted(Comparator.comparing(Person::getName))
+        persons.stream().filter(person -> person.getAge() >= 18).sorted(Comparator.comparing(Person::getName))
                 .forEach(System.out::println);
 
-        nums.stream().filter(person -> person.getAge() >=18).map(Person::getName)
+        persons.stream().filter(person -> person.getAge() >= 18).map(Person::getName)
                 .forEach(System.out::println);
+
+
+        System.out.println(persons.stream().filter(person -> person.getAge() >= 18).mapToInt(p -> p.getAge()).average().getAsDouble());
 
 
     }
